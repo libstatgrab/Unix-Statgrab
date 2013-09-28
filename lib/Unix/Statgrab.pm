9576d244e94cceeaccd304a878e5f09a28b8d733
Jens Rehsack <sno@netbsd.org>
2013-09-28 21:01:32 +0200

    bump version to 0.103

--- lib/Unix/Statgrab.pm
+++ lib/Unix/Statgrab.pm
@@ -85,7 +85,7 @@ my @constants_names = (
 	@constants_names
     );
 
-$VERSION = '0.102';
+$VERSION = '0.103';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
